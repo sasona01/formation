830983c32dc584c2a90e3bdb0f96ec8d03eaf05a
Sasona <samiressounaini@gmail.com>
2026-02-02 14:22:11 +0100

    ex1

--- geeks_institute_bootcamp/week_1/day_1/exercises/exercises_xp.js
+++ geeks_institute_bootcamp/week_1/day_1/exercises/exercises_xp.js
@@ -1,4 +1,22 @@
 // ===== Exercise 1
+const people = ["Greg", "Mary", "Devon", "James"];
+console.log(people)
+    people.splice(0,1);
+    console.log(people);
+        people[2]="Jason"
+        console.log(people);
+            people.push("sam");
+            console.log(people);
+                let newpeople=people.slice(1,3);
+                console.log(newpeople);
+                    let last=people[(people.length)-1];
+                    console.log(last);
+                        for(let i=0 ;i<people.length;i++){
+                        console.log(people[i]);
+                        if(people[i]=="Devon"){
+                            break;
+                        }
+                        }
 
 // ===== Exercise 2
 
