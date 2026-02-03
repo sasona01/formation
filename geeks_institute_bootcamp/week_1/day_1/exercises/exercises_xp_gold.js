75b1d00e40c71dd9be1c50fd7492c69318301fac
Sasona <samiressounaini@gmail.com>
2026-02-03 10:05:26 +0100

    exercises_xp_gold

--- geeks_institute_bootcamp/week_1/day_1/exercises/exercises_xp_gold.js
+++ geeks_institute_bootcamp/week_1/day_1/exercises/exercises_xp_gold.js
@@ -1,8 +1,45 @@
 // ===== Exercise 1
-
+/*
+let numbers = [123, 8409, 100053, 333333333, 7]
+for(let i=0;i<numbers.length;i++){
+    let x=numbers[i]%3;
+    if(x!=0)
+       { console.log('false' );}
+    else
+          {console.log('true' );}
+    
+}*/
 // ===== Exercise 2
+/*
+let x= prompt('entrer numbre :',);
+let guestList = {
+  randy: "Germany",
+  karla: "France",
+  wendy: "Japan",
+  norman: "England",
+  sam: "Argentina"
+}   
 
+if (x in guestList) {
+  alert("Hi! I'm " + x + ", and I'm from " + guestList[x] + ".");
+} else {
+  alert("Hi! I'm a guest.");
+}*/
 // ===== Exercise 3
+let age = [20,5,12,43,98,55];
+let x =0;
+let y =0;
+    for (i in age){
+    x=x+age[i]
+   
+    }
+        console.log("the sum of all the numbers in the age array is: "+x);
+    for (i in age){
+    if(age[i]>=y){ 
+        y=age[i];
+    }}
+        console.log("the highest age in the array is: " +y);
+
 
 // ===== Exercise 4
 
