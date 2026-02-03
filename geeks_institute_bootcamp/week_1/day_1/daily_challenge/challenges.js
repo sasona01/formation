a735dd8f46e938c8501e40fde4a0bc75a544a398
Sasona <samiressounaini@gmail.com>
2026-02-03 11:57:59 +0100

    Challenge Stars

--- geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,13 @@
-// ====== Daily Challenge 1
-
+// ====== Daily Challenge Stars
+ 
+ let x = 9;
+for(let i = 1; i <= x; i++) {
+    let l="";
+    for(let j = 0; j < i; j++) {
+        l += "*";
+    }
+    console.log(l);
+}
 // ====== Daily Challenge 2
 
 // ====== Daily Challenge 3
