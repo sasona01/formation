708f2a1518b7d2e52cdb5b750f894d27f1c83861
Sasona <samiressounaini@gmail.com>
2026-04-21 13:49:44 +0100

    Exrecises XP Gold

---
+++ geeks_institute_bootcamp/Week_5/day4/Exrecises_XP_Gold.js
@@ -0,0 +1,14 @@
+/*
+1.SELECT first_name, last_name, birth_date
+FROM students
+ORDER BY last_name ASC
+LIMIT 4;
+
+2.SELECT first_name, last_name, birth_date
+FROM students
+ORDER BY birth_date DESC
+LIMIT 1;
+
+3.SELECT first_name, last_name, birth_date
+FROM students
+LIMIT 3 OFFSET 2;
