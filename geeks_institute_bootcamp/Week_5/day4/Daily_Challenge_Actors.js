38a74880e62603822754bc54db57b7c7a5283f9d
Sasona <samiressounaini@gmail.com>
2026-04-21 13:52:31 +0100

    Exrecises XP Gold

---
+++ geeks_institute_bootcamp/Week_5/day4/Daily_Challenge_Actors.js
@@ -0,0 +1,4 @@
+/*1.SELECT COUNT(*) FROM actors;
+2.Si les colonnes sont en NOT NULL, l’insertion échoue avec une erreur.
+Sinon, la ligne sera insérée avec des valeurs NULL, ce qui n’est généralement pas souhaitable.
+*/
